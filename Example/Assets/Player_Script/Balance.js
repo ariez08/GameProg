08019112ef6a7026531c6738d4715983bcdd50dd
Dimas Irfan <dimaswongkoedoes@gmail.com>
2022-12-26 22:38:32 +0700

    Ragdoll+ScriptError

---
+++ Example/Assets/Player_Script/Balance.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var restingAngle : float = 0f;
+var force : float = 750f;
+var rb : Rigidbody2D ;
+function start() {
+  rb = gameObject.GetComponent("RigidBody2D");
+}
+
+function fixedUpdate() {
+  rb.MoveRotation(Mathf.LerpAngle(rb.rotation, restingAngle, force * Time.deltaTime));
+};
